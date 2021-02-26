25333e9c621e76272a1f6c1b541a7d7088970678
julián Boetto <malostratoscba@gmail.com>
2021-02-25 22:51:07 -0300

    Melhora pessoas adicionadas - feito pessoas maiore

--- ex04.js
+++ ex04.js
@@ -1,41 +1,93 @@
-function addNewTask() {
-    let nome = document.querySelector('#newNome');
-    let sobreNome = document.querySelector('#newSobre');
-    let idade = document.querySelector('#newIdade');
-    let taskList = document.querySelector('#taskList')
+let nomes = document.querySelector("#nome");
+let sobreNomes = document.querySelector("#sobreNome");
+let idades = document.querySelector("#idade");
+let form = document.querySelector("form");
+let btnExibirMaisVelhos = document.querySelector("#exibirMaisVelhos");
+let resultado = document.querySelector("#resultado")
 
-    let newNome = nome.value;
-    let newSobre = sobreNome.value;
-    let newIdade = idade.value;
+
+let pessoas = [];
+
+//Adiciona pessoas
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let pessoa = {
+    nome: nomes.value,
+    sobreNome: sobreNomes.value,
+    idade: Number(idades.value)
+  };
+
+  pessoas.push(pessoa);
+
+  nomes.value = "";
+  sobreNomes.value = "";
+  idades.value = "";
+  
+  //Cria a lista com o nome completo das pessoas
+  
+  resultado.textContent = `${pessoa.nome} ${pessoa.sobreNome} de ${pessoa.idade}`;
+});
+
+
+//Botao para exibir as pessoas maiores
+btnExibirMaisVelhos.addEventListener("click", function exibirMaisVelhos() {
+    let lista = document.createElement("ul");
+  
+    for (let pessoaMaisVelha of pessoasMaisVelhas(pessoas)) {
+      let item = document.createElement("li");
+      item.textContent = `${pessoaMaisVelha.nome} com ${pessoaMaisVelha.idade} anos`;
+      lista.appendChild(item);
+    }
+  
+    resultado.innerHTML = "";
+    resultado.appendChild(lista);
+  });
+  
+  function pessoasMaisVelhas(pessoas) {
+    let maisVelhasAtuais = [pessoas[0]];
+  
+    for (let pessoa of pessoas.slice(1)) {
+      if (maisVelhasAtuais[0].idade < pessoa.idade) {
+        maisVelhasAtuais = [pessoa];
+      } else if (maisVelhasAtuais[0].idade === pessoa.idade) {
+        maisVelhasAtuais.push(pessoa);
+      }
+    }
+  
+    return maisVelhasAtuais;
+  }
+
+//     // //Pega os valores
+//     // let newNome = nome.value;
+//     // nomes.push(newNome);
+//     // nome.value = "";
+//     // let newSobre = sobreNome.value;
+//     // sobreNomes.push(newSobre);
+//     // sobreNome.value = "";
+//     // let newIdade = idade.value;
+//     // idades.push(newIdade);
+//     // idade.value = "";
     
-    // let listNome = document.createElement("li");
-    // listNome.textContent = newNome;
-    // let listSobre = document.createElement("li");
-    // listSobre.textContent = newSobre;
-    // let listIdade = document.createElement("li");
-    // listIdade.textContent = newIdade;
+//     // //Elabora os dados completos
+//     // let nomeCompleto = newNome + " " + newSobre + " de " + newIdade + " anos" ;
+//     // //Crea o elemento
+//     // let listNomeCompleto = document.createElement("li");
+//     // listNomeCompleto.textContent = nomeCompleto;
 
-    let nomeCompleto = newNome + " " + newSobre;
+//     // //Adiciona o elemento ao final do formulario
+//     // taskList.appendChild(listNomeCompleto);  
 
-    let listNomeCompleto = document.createElement("li");
-    listNomeCompleto.textContent = nomeCompleto;
-
-    taskList.appendChild(listNomeCompleto);
-
-    // taskList.appendChild(listNome);
-    // taskList.appendChild(listSobre);
-    // taskList.appendChild(listIdade);
-}
-
-
-
-
-
-maior = 0;
-
-for(var i=0,len=idades .length;i<len;i++){
- if(maior < idades [i]){
-     maior = idades [i];
-}
-}
-console.log("A pessoa maior tem " + maior + " anos");
+ 
+// btnPessoaMaior.addEventListener("click", function exibirMaisVelhos() {
+//     let lista = document.createElement("ul");
+  
+//     for (let pessoaMaisVelha of pessoasMaisVelhas(pessoas)) {
+//       let item = document.createElement("li");
+//       item.textContent = `${pessoaMaisVelha.nome} com ${pessoaMaisVelha.idade} anos`;
+//       lista.appendChild(item);
+//     }
+  
+//     taskList.innerHTML = "";
+//     taskList.appendChild(lista);
+// });
